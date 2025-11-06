fd262ee7ceb8a8228765f8d3b8f7b08ae8793902
Georgi Hristov <georgi.dh@gmail.com>
2025-11-06 16:43:22 +0200

    Create dbContext and install SQLite

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MyMovieLibrary.Models; // Добави using за твоите модели
+
+namespace MyMovieLibrary.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        // Тази опция ще "инжектираме" от Program.cs
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+
+        // Това казва на EF Core: "Искам таблица, наречена Movies,
+        // която да използва моя Movie модел."
+        public DbSet<Movie> Movies { get; set; }
+    }
+}
