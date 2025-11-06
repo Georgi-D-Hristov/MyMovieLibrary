97f0bf762ab7364440d9bd47314bbb903c152057
Georgi Hristov <georgi.dh@gmail.com>
2025-11-06 16:56:25 +0200

    Modify MoviesController to use the new database.

--- Controllers/MoviesController.cs
+++ Controllers/MoviesController.cs
@@ -1,36 +1,33 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using MyMovieLibrary.Data;
 using MyMovieLibrary.Models; // <-- 1. Добави using за твоите модели
 
 namespace MyMovieLibrary.Controllers
 {
     public class MoviesController : Controller
     {
-        // --- Това е нашата "фалшива" база данни ---
-        // "static" означава, че списъкът ще е един и същ за всички потребители
-        // и няма да се нулира при всяка заявка.
-        private static List<Movie> _movies = new List<Movie>
-        {
-            new Movie { Id = 1, Title = "The Shawshank Redemption", Director = "Frank Darabont", Year = 1994, Genre = "Drama", Rating = 9 },
-            new Movie { Id = 2, Title = "The Godfather", Director = "Francis Ford Coppola", Year = 1972, Genre = "Crime", Rating = 9 },
-            new Movie { Id = 3, Title = "The Dark Knight", Director = "Christopher Nolan", Year = 2008, Genre = "Action", Rating = 9 }
-        };
-        // ---------------------------------------------
+        private readonly ApplicationDbContext _context;
 
+        public MoviesController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
 
         // Този метод (Action) ще се изпълни, когато отидеш на /Movies
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
             // 2. Вземаме всички филми от нашия "фалшив" списък
-            List<Movie> allMovies = _movies;
+            var allMovies = await _context.Movies.ToListAsync();
 
             // 3. Подаваме ги към View-то
             return View(allMovies);
         }
 
-        public IActionResult Details(int id)
+        public async Task<IActionResult> Details(int id)
         {
             // Намираме филма по ID
-            Movie movie = _movies.FirstOrDefault(m => m.Id == id);
+            Movie movie =await _context.Movies.FirstOrDefaultAsync(m => m.Id == id);
             if (movie == null)
             {
                 return NotFound(); // Ако не е намерен, връщаме 404
@@ -50,15 +47,11 @@ namespace MyMovieLibrary.Controllers
         // Този метод приема данните от изпратената форма
         [HttpPost] // Този атрибут казва, че методът отговаря само на POST заявки
         [ValidateAntiForgeryToken] // Добавя защита срещу CSRF атаки
-        public IActionResult Create(Movie movie)
+        public Task<IActionResult> Create(Movie movie)
         {
-            // 1. Изчисляваме ново ID. (В истинска база данни, това става автоматично)
-            // Взимаме най-голямото ID досега и добавяме 1.
-            int newId = _movies.Any() ? _movies.Max(m => m.Id) + 1 : 1;
-            movie.Id = newId;
+            _context.Movies.Add(movie);
 
-            // 2. Добавяме новия филм към нашия "фалшив" списък
-            _movies.Add(movie);
+            await _context.SaveChangesAsync();
 
             // 3. Пренасочваме потребителя обратно към списъка (Index)
             return RedirectToAction("Index");
