7bd29a0ed64b666d82a1753c8eef4d20ebbd7c41
Georgi Hristov <georgi.dh@gmail.com>
2025-11-06 16:08:44 +0200

    Create project

---
+++ Controllers/MoviesController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MyMovieLibrary.Models; // <-- 1. Добави using за твоите модели
+
+namespace MyMovieLibrary.Controllers
+{
+    public class MoviesController : Controller
+    {
+        // --- Това е нашата "фалшива" база данни ---
+        // "static" означава, че списъкът ще е един и същ за всички потребители
+        // и няма да се нулира при всяка заявка.
+        private static List<Movie> _movies = new List<Movie>
+        {
+            new Movie { Id = 1, Title = "The Shawshank Redemption", Director = "Frank Darabont", Year = 1994, Genre = "Drama", Rating = 9 },
+            new Movie { Id = 2, Title = "The Godfather", Director = "Francis Ford Coppola", Year = 1972, Genre = "Crime", Rating = 9 },
+            new Movie { Id = 3, Title = "The Dark Knight", Director = "Christopher Nolan", Year = 2008, Genre = "Action", Rating = 9 }
+        };
+        // ---------------------------------------------
+
+
+        // Този метод (Action) ще се изпълни, когато отидеш на /Movies
+        public IActionResult Index()
+        {
+            // 2. Вземаме всички филми от нашия "фалшив" списък
+            List<Movie> allMovies = _movies;
+
+            // 3. Подаваме ги към View-то
+            return View(allMovies);
+        }
+
+        public IActionResult Details(int id)
+        {
+            // Намираме филма по ID
+            Movie movie = _movies.FirstOrDefault(m => m.Id == id);
+            if (movie == null)
+            {
+                return NotFound(); // Ако не е намерен, връщаме 404
+            }
+            // Подаваме филма към View-то
+            return View(movie);
+        }
+    }
+}
---
+++ Models/Movie.cs
@@ -0,0 +1,12 @@
+﻿namespace MyMovieLibrary.Models
+{
+    public class Movie
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Director { get; set; }
+        public int Year { get; set; }
+        public string Genre { get; set; }
+        public int Rating { get; set; } // Оценка от 1 до 10
+    }
+}
